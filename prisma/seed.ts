4053b8496ab0152ec4e91b69cd7bb63621e0311f
Eduardo Ventura <ejvn30@gmail.com>
2026-03-10 14:59:24 +0000

    fix: add husky and schema prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
+import 'dotenv/config';
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  console.log('--- Iniciando Seed con SQL Nativo ---'); // eslint-disable-line
+}
+
+main()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
